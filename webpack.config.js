57da18b83f4f81ea5f2380c6f9333228835759af
Chris Fullinwider <cfullinw34@gmail.com>
2017-11-18 10:08:56 -0700

    use HtmlWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 // node path module
 const path = require('path');
-
+// let htmlWebpackPlugin handle creating html files for webpack bundle
+const htmlWebpackPlugin = require('html-webpack-plugin');
 
 const paths = {
 	DIST: path.resolve(__dirname, 'dist'),
@@ -15,8 +16,15 @@ module.exports = {
 		path: paths.DIST,
 		filename: 'app.bundle.js'
 	},
+	plugins: [
+		new htmlWebpackPlugin({
+			template: path.join(paths.SRC, 'index.html')
+		})
+	]
+
 	// use src folder as starting point for dev server
-	devServer: {
-		contentBase: paths.SRC
-	}
+	// NOT NEEDED DUE TO HTML WEBPACK PLUGIN
+	// devServer: {
+	// 	contentBase: paths.SRC
+	// }
 };
